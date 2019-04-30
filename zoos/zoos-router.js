57d2daa52d715684e57aa5e3f3f3600d8322718e
AJLambda <apj2277@gmail.com>
2019-04-30 14:23:07 -0700

    moving everything into index

--- zoos/zoos-router.js
+++ zoos/zoos-router.js
@@ -11,3 +11,16 @@ const knexConfig = {
   //debug: true,
 };
 const db = knex(knexConfig);
+
+// GET /api/zoos
+router.get("/", (req, res) => {
+  // select * from roles
+  db("zoos") //<< return a promise with all the rows
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
